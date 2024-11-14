4f54e439f293f66fc07ee479fe8d1f571dc23e8d
Wave Tanadol <wave70000@hotmail.com>
2024-11-14 13:14:34 +0700

    update code

--- src/main/java/com/github/wzzk/SslClient.java
+++ src/main/java/com/github/wzzk/SslClient.java
@@ -1,4 +1,40 @@
 package com.github.wzzk;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+
 public class SslClient {
+
+    public static void main(String[] args) throws Exception {
+        KeyStore trustStore = KeyStore.getInstance("JKS");
+        try (FileInputStream trustStoreFile = new FileInputStream("src/main/resources/client-truststore.jks")) {
+            trustStore.load(trustStoreFile, "p@ssw0rd".toCharArray());
+        }
+
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("SunX509");
+        trustManagerFactory.init(trustStore);
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustManagerFactory.getTrustManagers(), null);
+
+        SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+        try (SSLSocket socket = (SSLSocket) sslSocketFactory.createSocket("localhost", 8443)) {
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(),true);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            writer.println("Hello from SSL Client!");
+            String serverResponse = reader.readLine();
+            System.out.println("Received from server: " + serverResponse);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- src/main/java/com/github/wzzk/SslSocket.java
+++ src/main/java/com/github/wzzk/SslSocket.java
@@ -1,4 +1,45 @@
 package com.github.wzzk;
 
+import javax.net.ssl.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+
 public class SslSocket {
+
+    public static void main(String[] args) throws Exception {
+
+        KeyStore keyStore = KeyStore.getInstance("JKS");
+        try (FileInputStream keyStoreFile = new FileInputStream("src/main/resources/server.keystore")) {
+            keyStore.load(keyStoreFile, "p@ssw0rd".toCharArray());
+        }
+
+        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
+        keyManagerFactory.init(keyStore, "p@ssw0rd".toCharArray());
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(keyManagerFactory.getKeyManagers(), null, null);
+
+        SSLServerSocketFactory serverSocketFactory = sslContext.getServerSocketFactory();
+        SSLServerSocket serverSocket = (SSLServerSocket) serverSocketFactory.createServerSocket(8443);
+
+        System.out.println("SSL Server started and listening on port 8443...");
+        while (true) {
+            try (SSLSocket clientSocket = (SSLSocket) serverSocket.accept()) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                String clientMessage = reader.readLine();
+                System.out.println("Received: " + clientMessage);
+
+                writer.println("Hello from SSL Server!");
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
 }
